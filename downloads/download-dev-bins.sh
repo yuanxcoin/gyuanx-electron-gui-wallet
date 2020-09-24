eef5c1eb109a279e9f03bbed4f91a16aee116d76
Kyle Zsembery <zsembery.kyle@gmail.com>
2020-09-24 18:59:07 +1000

    add CI for dev bins

---
+++ downloads/download-dev-bins.sh
@@ -0,0 +1,25 @@
+set -e
+
+if [ -z "$OS" ]; then
+    echo "OS must be set"
+    exit 1
+fi
+
+if [ -z "$RENAME" ]; then
+    RENAME="latest"
+fi
+
+URL="https://builds.lokinet.dev/loki-project/loki-core/"
+
+if [ "$OS" == "Linux" ]; then
+    FILE_OS="linux"
+elif [ "$OS" == "Windows" ]; then
+    FILE_OS="win"
+elif [ "$OS" == "macOS" ]; then
+    FILE_OS="macos"
+else
+    echo "OS must be Linux, Windows or macOS"
+    exit 1
+fi
+
+wget "https://builds.lokinet.dev/loki-project/loki-core/loki-dev-${FILE_OS}-LATEST.tar.xz" -O ${RENAME}

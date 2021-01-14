272b66990745dd3f3eb5ca6addd584cc9202b66e
das <saz@gmail.com>
2021-01-14 16:00:04 +0000

    changed all files

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: true,
-  singleQuote: false,
-  useTabs: false,
-  trailingComma: "none",
-  printWidth: 80
-};
--- src/pages/404.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="fixed-center text-center">
-    internal error
-  </div>
-</template>
